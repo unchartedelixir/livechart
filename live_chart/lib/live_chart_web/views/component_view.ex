dcfd48f3358c0169f0ad93762e24d5444d1be6f4
Tim Mecklem <timothy@mecklem.com>
2020-09-12 09:24:14 -0400

    Break out SVG into leex template files

---
+++ live_chart/lib/live_chart_web/views/component_view.ex
@@ -0,0 +1,10 @@
+defmodule LiveChartWeb.ComponentView do
+  use Phoenix.View,
+    root: "lib/live_chart_web/components",
+    namespace: LiveChartWeb,
+    path: ""
+
+  use Phoenix.HTML
+
+  alias LiveChart.{Chart}
+end
