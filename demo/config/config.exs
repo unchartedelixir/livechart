de548d24a4ac2ae46a34168a1185123c4f7c0f28
Tim Mecklem <timothy@mecklem.com>
2020-09-11 21:38:17 -0400

    Add demo app that depends on the live_chart package

---
+++ demo/config/config.exs
@@ -0,0 +1,29 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :demo, DemoWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "P/mGbaiNfMmv6yaOKDn/sKJQp8qqgdnNdw7RKkDuwHOvPCgYG9k21clFWTC8U4YJ",
+  render_errors: [view: DemoWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Demo.PubSub,
+  live_view: [signing_salt: "wPpdIkS0"],
+  reloadable_apps: [:demo, :live_chart]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
