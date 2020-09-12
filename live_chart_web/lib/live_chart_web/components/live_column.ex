5505a25ed7fa0c425314a73f3ca01fa00519b64d
Zack Kayser <zkayser@gmail.com>
2020-09-12 16:31:55 -0400

    Pass grid_lines and grid_line_offsetter function as assigns to live chart component view

--- live_chart_web/lib/live_chart_web/components/live_column.ex
+++ live_chart_web/lib/live_chart_web/components/live_column.ex
@@ -2,7 +2,19 @@ defmodule LiveChartWeb.LiveColumnComponent do
   use Phoenix.LiveComponent
 
   def update(assigns, socket) do
-    {:ok, assign(socket, chart: assigns.chart, columns: LiveChart.ColumnChart.columns(assigns.chart))}
+    y_axis = assigns.chart.dataset.axes.y
+    # Hardcode the number of steps to take as 5 for now
+    grid_lines = y_axis.grid_lines.({y_axis.min, y_axis.max}, 5)
+    grid_line_offsetter = fn grid_line -> 100 * (y_axis.max - grid_line) / y_axis.max end
+
+    socket =
+      socket
+      |> assign(:chart, assigns.chart)
+      |> assign(:columns, LiveChart.ColumnChart.columns(assigns.chart))
+      |> assign(:grid_lines, grid_lines)
+      |> assign(:grid_line_offsetter, grid_line_offsetter)
+
+    {:ok, socket}
   end
 
   def render(assigns) do
