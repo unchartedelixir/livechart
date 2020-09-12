09e5538dac2477513b0b0db7d8f257a24330957f
Zack Kayser <zkayser@gmail.com>
2020-09-12 19:46:33 -0400

    Require a chart to be passed to LiveProgress component

--- live_chart_web/lib/live_chart_web/components/live_progress.ex
+++ live_chart_web/lib/live_chart_web/components/live_progress.ex
@@ -1,6 +1,12 @@
 defmodule LiveChartWeb.LiveProgressComponent do
   use Phoenix.LiveComponent
 
+  def update(assigns, socket) do
+    socket = assign(socket, :chart, assigns.chart)
+
+    {:ok, socket}
+  end
+
   def render(assigns) do
     Phoenix.View.render(LiveChartWeb.ComponentView, "live_progress.html", assigns)
   end
--- live_chart_web/test/live_chart_web/components/live_progress_test.exs
+++ live_chart_web/test/live_chart_web/components/live_progress_test.exs
@@ -1,14 +1,22 @@
 defmodule LiveChartWeb.LiveProgressComponentTest do
+  alias LiveChart.BaseChart
   alias LiveChartWeb.LiveProgressComponent
   import Phoenix.LiveViewTest
   use ExUnit.Case
 
   @endpoint NotAThingYet
+  @chart %BaseChart{
+    title: "my progress chart"
+  }
 
   describe "LiveProgress component" do
     test "render/1 mounts successfully" do
-      assert render_component(LiveProgressComponent, []) =~
+      assert render_component(LiveProgressComponent, chart: @chart) =~
                ~s(data-testid="lc-live-progress-component")
+    end
+
+    test "renders title for accessibility" do
+      assert render_component(LiveProgressComponent, chart: @chart) =~ @chart.title
     end
   end
 end
