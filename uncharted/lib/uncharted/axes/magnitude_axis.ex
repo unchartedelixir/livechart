e987732bcc24a68a25b35c59762a57be32c71546
Zack Kayser <zkayser@gmail.com>
2020-09-13 18:49:51 -0400

    Document MagnitudeAxis.default_grid_lines_fun/2

--- uncharted/lib/uncharted/axes/magnitude_axis.ex
+++ uncharted/lib/uncharted/axes/magnitude_axis.ex
@@ -23,6 +23,15 @@ defmodule Uncharted.Axes.MagnitudeAxis do
           grid_lines: grid_lines_func
         }
 
+  @doc """
+  The default function used to determine spacial offsets of labels
+  along an axis, as well as `grid_lines` going across a chart.
+
+  If you do not specify a different `grid_lines_func` function
+  when you create a `Uncharted.Axes.MagnitudeAxis` struct, this
+  implementation will be provided used.
+  """
+  @spec default_grid_lines_fun({min, max}, step) :: list(Float.t())
   def default_grid_lines_fun({min, max}, step) do
     min..max
     |> Enum.take_every(div(max - min, step))
