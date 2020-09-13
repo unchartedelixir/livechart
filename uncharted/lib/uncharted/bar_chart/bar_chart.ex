81d6d16075201da110af1e8fbee0b5f6355d6ed6
Tim Mecklem <timothy@mecklem.com>
2020-09-13 19:46:27 -0400

    Document the bar chart protocol

--- uncharted/lib/uncharted/bar_chart/bar_chart.ex
+++ uncharted/lib/uncharted/bar_chart/bar_chart.ex
@@ -1,5 +1,8 @@
 defprotocol Uncharted.BarChart do
   @spec bars(Uncharted.chart() | Uncharted.BarChart.Dataset.t()) ::
           list(Uncharted.BarChart.Bar.t())
+  @doc """
+  Returns a list of the `Uncharted.BarChart.Bar.t()` that should be rendered by the chart display adapter
+  """
   def bars(chart)
 end
