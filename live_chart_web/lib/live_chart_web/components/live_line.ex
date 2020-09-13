4ecf29a40b23980ab9745f57114b0ee8d162614e
Tim Mecklem <timothy@mecklem.com>
2020-09-12 21:30:38 -0400

    Split out line and bar charts into components and templates (not functioning yet)

---
+++ live_chart_web/lib/live_chart_web/components/live_line.ex
@@ -0,0 +1,23 @@
+defmodule LiveChartWeb.LiveLineComponent do
+  use Phoenix.LiveComponent
+
+  def update(assigns, socket) do
+    # y_axis = assigns.chart.dataset.axes.y
+    # # Hardcode the number of steps to take as 5 for now
+    # grid_lines = y_axis.grid_lines.({y_axis.min, y_axis.max}, 5)
+    # grid_line_offsetter = fn grid_line -> 100 * (y_axis.max - grid_line) / y_axis.max end
+
+    socket =
+      socket
+      |> assign(:chart, assigns.chart)
+      # |> assign(:columns, LiveChart.ColumnChart.columns(assigns.chart))
+      # |> assign(:grid_lines, grid_lines)
+      # |> assign(:grid_line_offsetter, grid_line_offsetter)
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    Phoenix.View.render(LiveChartWeb.ComponentView, "live_line.html", assigns)
+  end
+end
