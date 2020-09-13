ef3a0a2fb07362867a5755a7d437fdf613103560
Zack Kayser <zkayser@gmail.com>
2020-09-12 22:08:44 -0400

    Assert that LiveProgressComponent renders progress

--- live_chart_web/test/live_chart_web/components/live_progress_test.exs
+++ live_chart_web/test/live_chart_web/components/live_progress_test.exs
@@ -1,12 +1,24 @@
 defmodule LiveChartWeb.LiveProgressComponentTest do
   alias LiveChart.BaseChart
+  alias LiveChart.ProgressChart
+  alias LiveChart.ProgressChart.Dataset
   alias LiveChartWeb.LiveProgressComponent
   import Phoenix.LiveViewTest
   use ExUnit.Case
 
   @endpoint NotAThingYet
   @chart %BaseChart{
-    title: "my progress chart"
+    title: "my progress chart",
+    colors: %{gray: "#e2e2e2"},
+    dataset: %Dataset{
+      background_stroke_color: :gray,
+      label: "Unchartedness",
+      to_value: 100,
+      current_value: 45,
+      percentage_text_fill_color: :red_gradient,
+      percentage_fill_color: :rosy_gradient,
+      label_fill_color: :rosy_gradient
+    }
   }
 
   describe "LiveProgress component" do
@@ -17,6 +29,11 @@ defmodule LiveChartWeb.LiveProgressComponentTest do
 
     test "renders title for accessibility" do
       assert render_component(LiveProgressComponent, chart: @chart) =~ @chart.title
+    end
+
+    test "it renders the percentage" do
+      assert render_component(LiveProgressComponent, chart: @chart) =~
+               "#{ProgressChart.progress(@chart)}"
     end
   end
 end
